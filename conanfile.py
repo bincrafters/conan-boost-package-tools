95208e1994d76966baaf92fe5160516c9f2b3189
Jerry W <jerrywiltse@gmail.com>
2017-12-28 21:36:21 -0500

    initial commit

---
+++ conanfile.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from conans import ConanFile
+
+
+class BoostPackageToolsConan(ConanFile):
+    name = "boost_package_tools"
+    version = "1.65.1"
+    url = "https://github.com/bincrafters/conan-boost-package-tools"
+    description = "Shared python code used in other Conan recipes for the Boost libraries"
+    license = "MIT"
+    exports = "boost-package-tools.py", "LICENSE.md"
+    
+    def package(self):
+        self.copy('boost-package-tools.py')
+        self.copy('LICENSE.md')
+
+    def package_info(self):
+        self.env_info.PYTHONPATH.append(self.package_folder)
+    
+    
